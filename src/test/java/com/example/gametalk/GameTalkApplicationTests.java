cc41bf29a698a6cce29947cf4876b8128c3d1085
newbiekk <newbiekk1126@gmail.com>
2024-11-19 12:05:45 +0900

    Feat : GameTalk 프로젝트 생성
    프로젝트 생성

---
+++ src/test/java/com/example/gametalk/GameTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gametalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
