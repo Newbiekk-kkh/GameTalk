ed031d8161870ba605e5dce75702ec42ce0777f2
Park JaeHong <blogerhong@naver.com>
2024-11-20 13:49:30 +0900

    fix : UserRepository 사용하지 않는 메서드 제거

--- src/main/java/com/example/gametalk/repository/UserRepository.java
+++ src/main/java/com/example/gametalk/repository/UserRepository.java
@@ -8,12 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    /**
-     * @apiNote 저장된 유저 데이터 중 같은 email 이 있는지 체크
-     * @param email
-     * @return 중복되면 true 중복되지 않으면 false 반환
-     */
-    boolean existsByEmail(String email);
-
     Optional<User> findByEmail(String email);
 }
